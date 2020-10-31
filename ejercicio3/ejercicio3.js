008414b394e037d85363e19689d50020c5091f90
sebastiangr10 <sebastiangr1091@gmail.com>
2020-10-31 17:03:46 -0500

    ejercicio3

---
+++ ejercicio3/ejercicio3.js
@@ -0,0 +1,15 @@
+let juanEdad
+let albertoEdad
+let anaEdad
+let mamaEdad
+juanEdad=Number(prompt("¿cuál es la edad de juan?"));
+albertoEdad=parseInt((juanEdad*2)/3);
+anaEdad=parseInt((juanEdad*4)/3);
+mamaEdad=(juanEdad+albertoEdad+anaEdad);
+alert("la edad de la familia es "+ "mamá tiene " +mamaEdad+ "años")
+alert("Juan tiene "+ juanEdad+ "años")
+alert("Alberto tiene "+albertoEdad+ "años")
+alert("Ana tiene "+ anaEdad+"años")
+
+
+    
